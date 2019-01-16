690733e0df3dfd781df96d9c9547bf895b6898a9
mariatiganoiu <44686357+mariatiganoiu@users.noreply.github.com>
2019-01-16 21:11:46 +0200

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int a, n, r = 1;
+	//in r vom salva rezultatul final
+	//se citesc a si n
+	cin >> a >> n;
+	while (n > 0)
+	{
+		r *= a;
+		//se inmulteste a la valoarea salvata anterior in r
+		n--;
+		//se micsoreaza n, apoi se va verifica din nou daca n e pozitiv
+	}
+	//atfel se efectueaza instructiunile din while de n ori
+	cout << r << endl;
+	system("pause");
+	return 0;
+}
